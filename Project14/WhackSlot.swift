e46782894ca1f262602ac256e07aeab40a9d3735
Andre <katsuya.tsu@gmail.com>
2021-07-29 15:02:22 -0300

    added sprites assets, and penguins and holes to the view. Added crop hole using SKSpriteNode

---
+++ Project14/WhackSlot.swift
@@ -0,0 +1,33 @@
+//
+//  WhackSlot.swift
+//  Project14
+//
+//  Created by user on 29/07/21.
+//
+
+import UIKit
+import SpriteKit
+
+class WhackSlot: SKNode {
+    
+    var charNode: SKSpriteNode!
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = nil
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+}
